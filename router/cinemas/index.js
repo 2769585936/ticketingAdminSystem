06d492bdc4e528b837867e0d6a6861cac87aab75
叶子祥 <yezixiang@aa.com>
2024-05-23 21:28:21 +0800

    修改

--- router/cinemas/index.js
+++ router/cinemas/index.js
@@ -27,6 +27,25 @@ cinemasRouter.get('/cinemas', async (req, res) => {
     msg: 'ok'
   })
 })
+
+// 根据_id 获取信息
+cinemasRouter.get('/cinemasid', async (req, res) => {
+  // 这个id是电影ID 根据电影获取影城
+  const { _id } = req.query
+
+  let data
+  if (_id) {
+    data = await MyFilmSessionsModel.find({ _id }).populate(['_cid', '_fid'])
+  } else {
+    data = await MyCinemasModel.find()
+  }
+
+  return res.send({
+    code: '0000',
+    data: data,
+    msg: 'ok'
+  })
+})
 // 获取根据_fid 和_cid 获取有几个场次
 cinemasRouter.get('/cinemasTime', async (req, res) => {
   const { _cid, _fid } = req.query
