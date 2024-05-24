fb9ee88a5607010669a827c74dbdbde61a8262b4
叶子祥 <yezixiang@aa.com>
2024-05-24 21:20:40 +0800

    giugai

--- daorubao/bc/users.js
+++
@@ -1,8 +0,0 @@
-db.getCollection("users").insert( {
-    __v: "0",
-    _id: "664bf30714352ec40e7bea45",
-    name: "yzx",
-    password: "25d55ad283aa400af464c76d713c07ad",
-    phone: 17720349026,
-    userPicture: null
-} );
